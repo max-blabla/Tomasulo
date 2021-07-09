40fe3df478d7bc45045971a33c38c8824dc36f9c
max-blabla <74357554+max-blabla@users.noreply.github.com>
2021-07-09 16:35:58 +0800

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/riscv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/riscv.dir/main.cpp.o"
+  "riscv.pdb"
+  "riscv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/riscv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
